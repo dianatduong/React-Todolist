4bb4a5e4491491f0f31750561977edfdc34e697b
DianaDee <dduong013@gmail.com>
2021-02-02 14:10:05 -0600

    playing with useState

--- src/App.js
+++ src/App.js
@@ -1,49 +1,49 @@
-import React, {Component} from 'react'
+import React, { useState } from 'react'
 import Categories from './Components/Categories'
 import './App.css'
 
-class App extends Component {
+const App = props => {
 
-    state = {
+  const [ categoriesState, setCategoriesState  ] = useState ({
+      categories: [
+         { name: "Produce" },
+         { name: "Meat" },
+         { name: "Dairy" },
+         { name: "Frozen" },
+         { name: "Pantry"}
+       ]
+  });
+    
+   const switchCategoryNameHandler = () => {
+    setCategoriesState ({
         categories: [
-           { name: "Produce" },
-           { name: "Meat" },
-           { name: "Dairy" },
-           { name: "Frozen" },
-           { name: "Pantry"}
-         ]
-    }
-
-    toggleHandler = () => {
-      //console.log("Button clicked");
-      const categoryBtn = document.getElementById('categoryId');
-      const inputBox = document.createElement('input');
-            inputBox.setAttribute('type', 'text');
-            inputBox.placeholder = "add product"
-      categoryBtn.parentNode.insertBefore(inputBox, categoryBtn.nextSibling);
-    }
-
-
-    render () {
+          { name: "Dessert" },
+          { name: "Bakery" },
+          { name: "Deli" },
+          { name: "Frozen" },
+          { name: "Pantry"}
+        ]
+    })
+  }
         return (
             <div className="container">
                 <Categories
-                  name={this.state.categories[0].name} 
-                  click={this.toggleHandler}/><br />
+                  name={categoriesState.categories[0].name} 
+                  click={switchCategoryNameHandler}/><br />
                 <Categories 
-                  name={this.state.categories[1].name} 
-                  click={this.toggleHandler}/><br />
+                  name={categoriesState.categories[1].name} 
+                  click={switchCategoryNameHandler}/><br />
                 <Categories 
-                  name={this.state.categories[2].name} 
-                  click={this.toggleHandler}/><br />
+                  name={categoriesState.categories[2].name} 
+                  click={switchCategoryNameHandler}/><br />
                 <Categories 
-                  name={this.state.categories[3].name} 
-                  click={this.toggleHandler}/><br />
+                  name={categoriesState.categories[3].name} 
+                  click={switchCategoryNameHandler}/><br />
                 <Categories 
-                  name={this.state.categories[4].name} 
-                  click={this.toggleHandler}/>
+                  name={categoriesState.categories[4].name} 
+                  click={switchCategoryNameHandler}/>
             </div>  
         )
-    }
+    
 }
 export default App;
