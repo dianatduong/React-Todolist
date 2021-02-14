45dcf4dc3b33414d4ab53d2e0400afbddb25e41d
DianaDee <dduong013@gmail.com>
2021-02-13 20:19:56 -0600

    todolist

--- src/App.js
+++ src/App.js
@@ -1,49 +1,19 @@
 import React, {Component} from 'react'
-import Categories from './Components/Categories'
 import './App.css'
 
-class App extends Component {
+const App = () => {
 
-    state = {
-        categories: [
-           { name: "Produce" },
-           { name: "Meat" },
-           { name: "Dairy" },
-           { name: "Frozen" },
-           { name: "Pantry"}
-         ]
-    }
-
-    toggleHandler = () => {
-      //console.log("Button clicked");
-      const categoryBtn = document.getElementById('categoryId');
-      const inputBox = document.createElement('input');
-            inputBox.setAttribute('type', 'text');
-            inputBox.placeholder = "add product"
-      categoryBtn.parentNode.insertBefore(inputBox, categoryBtn.nextSibling);
-    }
-
-
-    render () {
-        return (
-            <div className="container">
-                <Categories
-                  name={this.state.categories[0].name} 
-                  click={this.toggleHandler}/><br />
-                <Categories 
-                  name={this.state.categories[1].name} 
-                  click={this.toggleHandler}/><br />
-                <Categories 
-                  name={this.state.categories[2].name} 
-                  click={this.toggleHandler}/><br />
-                <Categories 
-                  name={this.state.categories[3].name} 
-                  click={this.toggleHandler}/><br />
-                <Categories 
-                  name={this.state.categories[4].name} 
-                  click={this.toggleHandler}/>
-            </div>  
-        )
-    }
+  return (
+    <div>
+      <h1>Todo List</h1>
+        <input 
+          placeholder='add todo item'
+          type='text'
+          />
+          <button>Submit</button>
+    </div>
+  )
 }
+
+
 export default App;
